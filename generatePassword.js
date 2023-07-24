4b205351d62d4ad9b151d5da9a6b444678b8fd40
sparkfire298 <sparkfire298@gmail.com>
2023-07-24 11:41:26 +1000

    Add files via upload

---
+++ generatePassword.js
@@ -0,0 +1,29 @@
+function generatePassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    let password = "";
+    
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset.charAt(randomIndex);
+    }
+  
+    return password;
+  }
+  
+  function copyToClipboard(text) {
+    const input = document.createElement("input");
+    input.style.position = "fixed";
+    input.style.opacity = 0;
+    input.value = text;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand("copy");
+    document.body.removeChild(input);
+  }
+  
+  const passwordLength = 13;
+  const generatedPassword = generatePassword(passwordLength);
+  console.log("Here is your password:");
+  console.log("---------------------------------------");
+  console.log(generatedPassword);
+  console.log("---------------------------------------");
